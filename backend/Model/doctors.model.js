327ed9f33e975c74ce6d08f44abca915cc6a83bc
KULDEEP SINGH NEGI <nkuldeep615@gmail.com>
2023-06-14 23:20:30 +0530

    Appointmentroute

--- backend/Model/doctors.model.js
+++ backend/Model/doctors.model.js
@@ -3,15 +3,24 @@ const mongoose = require('mongoose');
 const DoctorsSchema = mongoose.Schema(
     {
         name: { type: String, required: true },
+        phone: { type: String, required: true },
         image: { type: String, required: true },
         about: { type: String, required: true },
         qualification: { type: String, required: true },
         experience: { type: String, required: true },
         language: { type: String, required: true },
-        licenceNumber: { type: String, required: true },
         charge: { type: String, required: true },
         timings: { type: String, required: true },
-
+        clinic: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Clinic"
+        },
+        appointments: [
+            {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "Appointment"
+            }
+        ]
     },
     { versionKey: false }
 )
