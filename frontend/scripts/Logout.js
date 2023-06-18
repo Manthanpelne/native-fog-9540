5236db4269637d11a9ac5fa13faecf2a15775725
unknown <shaikhshahbaz4022@gmail.com>
2023-06-19 01:15:05 +0530

    Admin Dashboard/filter Added

---
+++ frontend/scripts/Logout.js
@@ -0,0 +1,44 @@
+let Logout_anch = document.getElementById("Logout-anch")
+const BASE_SERVER_URL = "https://hilarious-bear-vestments.cyclic.app"
+Logout_anch.addEventListener("click", async () => {
+    try {
+        let res = await fetch(`${BASE_SERVER_URL}/user/logout`, {
+            method: "GET",
+            headers: {
+
+                "Content-Type": "application/json",
+                "Authorization": token
+            }
+        })
+        let data = await res.json()
+        console.log(data);
+        if (data.msg !== "Logout Success") {
+            Swal.fire({
+                position: "centre",
+                icon: "error",
+                title: `${data.msg}`,
+                showConfirmButton: false,
+                timer: 1500,
+            });
+        } else {
+            Swal.fire({
+                position: "centre",
+                icon: "success",
+                title: `${data.msg}`,
+                showConfirmButton: false,
+                timer: 1500,
+            });
+       
+            setTimeout(() => {
+                localStorage.clear("token")
+                window.location.reload()
+                window.location.href = "./index.html"
+            }, 2500);
+
+        }
+    } catch (error) {
+        console.log(error);
+    }
+
+
+})
